bd1a779414df2726aafd5f12d9853c21a0c92521
Colin Turner <c.turner@ulster.ac.uk>
2007-04-10 10:10:03 +0000

    + Added entry for cron job that closes vacancies where required
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@54 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

--- debian/cron.d.ex
+++ debian/cron.d.ex
@@ -1,4 +1,5 @@
 #
 # Regular cron jobs for the opus package
 #
-0 4	* * *	root	opus_maintenance
+#  m h  dom mon dow user command
+5 *	* * *	www-data	/usr/bin/php -q /usr/share/opus/cron/update_vacancy_status.php > /dev/null
