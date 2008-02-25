20dfca19c8659a87c46bff2d44c66fa8c907cc7c
Colin Turner <c.turner@ulster.ac.uk>
2008-02-25 14:17:47 +0000

    fixed authenticator for s prefix handling
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/branches/4.0.0@443 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

--- include/auth.d/40_ldap_uu.class.php
+++ include/auth.d/40_ldap_uu.class.php
@@ -35,57 +35,31 @@ class ldap_uu
 
     if ($ds)
     {
-        $username = strtolower($username);
+      $username = strtolower($username);
 
-        if (strcmp(substr($username,0,1),"s") == 0) {
-          $grp_array=array("student");
-          $reg_number = substr($username,1);
-        }
-        elseif ((ereg('[^0-9]', substr($username,0,1)))) {
-          $grp_array = array('academic');
-          $reg_number = $username;
-        }
-        else {
-          $reg_number = $username;
-          $username = 's'.$username;
-          $grp_array=array("student");
-        }
-
-        if (strlen($password) == 0) $password = "you have not entered a password so I am not letting you in!";
-        $r = @ldap_bind($ds, "uid=$username,ou=People,dc=ulst,dc=ac,dc=uk","$password");
-
-        $sr = @ldap_search($ds, "ou=People,dc=ulst,dc=ac,dc=uk", "uid=$username");
-        $entry = @ldap_first_entry($ds, $sr);
-        $attrs = @ldap_get_attributes($ds, $entry);
+      // Backwards compatibility for no prefix
+      if(preg_match("/^[0-9]+$/", $username)) $username = "s" . $username;
+      $r = @ldap_bind($ds, "uid=$username,ou=People,dc=ulst,dc=ac,dc=uk","$password");
+      $sr = @ldap_search($ds, "ou=People,dc=ulst,dc=ac,dc=uk", "uid=$username");
+      $entry = @ldap_first_entry($ds, $sr);
+      $attrs = @ldap_get_attributes($ds, $entry);
 
       if ($r === TRUE) 
       {
-        if ($reg_number != "") 
+        $auth_user['valid'] = true;
+        // Remove s for OPUS db
+        if(preg_match("/^s[0-9]+$/", $username))
         {
-            $firstname = $attrs["givenName"][0];
-            $lastname = $attrs["sn"][0];
-            $email = $attrs["mail"][0];
+          $auth_user['username'] = substr($username, 1);
         }
+        else
+        {
+          $auth_user['username'] = $username;
+        }
+        return($auth_user);
       }
-      else
-      {
-        // Nothing coming back
-        return false;
-      }
-    }
-
-    $auth_user['valid'] = True;
-    $auth_user['firstname'] = $firstname;
-    $auth_user['lastname'] = $lastname;
-    $auth_user['groups'] = $grp_array;
-    $auth_user['email'] = $email;
-    $auth_user['reg_number'] = $reg_number;
-    $auth_user['username'] = $username;
-
-    return $auth_user;
+    return false;
   }
-
-
 }
 
 ?>
