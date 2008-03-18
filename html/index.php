158fe98898b328bd8463e892b40d18c8b8149e29
Colin Turner <c.turner@ulster.ac.uk>
2008-03-18 15:14:55 +0000

    made the situation worse...
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@571 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

--- html/index.php
+++ html/index.php
@@ -946,39 +946,6 @@ function download_artefact(&$waf)
   }
 }
 
-function help_directory(&$waf)
-{
-  require_once("model/Admin.class.php");
-  require_once("model/HelpDirectory.class.php");
-
-  $student_id = $_SESSION['student_id'];
-  if(empty($student_id)) $student_id = WA::request("student_id");
-
-  if($student_id)
-  {
-    require_once("model/Student.class.php");
-    $student = Student::load_by_user_id($student_id);
-    $specific_admins = HelpDirectory::get_student_admins($student_id);
-    $waf->assign("student_id", $student_id);
-    $waf->assign("student", $student);
-  }
-
-  $root_admins = HelpDirectory::get_root_admins();
-  $inst_admins = HelpDirectory::get_institutional_admins();
-
-  $admin_headings = Admin::get_admin_list_headings();
-  $root_headings = Admin::get_root_list_headings();
-
-  $waf->assign("root_admins", $root_admins);
-  $waf->assign("inst_admins", $inst_admins);
-  $waf->assign("specific_admins", $specific_admins);
-  $waf->assign("admin_headings", $admin_headings);
-  $waf->assign("root_headings", $root_headings);
-
-  $waf->display("main.tpl", "admin:information:help_directory:help_directory", "admin/information/help_directory.tpl");
-}
-
-
 function get_academic_year()
 {
   global $config;
