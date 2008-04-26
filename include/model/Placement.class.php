5510ea60cf1509b2c7a485365c0a815071b7dea0
Colin Turner <c.turner@ulster.ac.uk>
2008-04-26 11:42:34 +0000

    removed some inert code and commented file
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@631 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

--- include/model/Placement.class.php
+++ include/model/Placement.class.php
@@ -33,6 +33,10 @@ class Placement extends DTO_Placement
   var $vacancy_id;            // The vacancy placed with
   var $student_id;            // The id, from the user table for the student
 
+
+  /**
+  * field definitions used to define how certain table fields should be treated
+  */ 
   static $_field_defs = array(
     'position'=>array('type'=>'text', 'size'=>30, 'maxsize'=>100, 'title'=>'Job Description','header'=>true, 'mandatory'=>true),
     'jobstart'=>array('type'=>'isodate', 'inputstyle'=>'popup', 'required'=>'true', 'title'=>'Job Start Date'),
@@ -50,17 +54,32 @@ class Placement extends DTO_Placement
     'student_id'=>array('type'=>'hidden')
   );
 
+  /**
+  * overrides to standard field definitions for admin users
+  * 
+  * @see $_field_defs
+  */
   static $_field_defs_admin_override = array(
     'supervisor_title'=>array('type'=>'text', 'size'=>5, 'maxsize'=>100, 'title'=>"Supervisor Title<br /><small>Mr, Dr, etc.</small>"),
     'supervisor_lastname'=>array('type'=>'text', 'size'=>20, 'maxsize'=>100, 'title'=>"Supervisor Last name")
   );
 
+  /**
+  * overrides to standard field definitions for supervisor users
+  * 
+  * @see $_field_defs
+  */
   static $_field_defs_supervisor_override = array(
     'position'=>array('type'=>'text', 'size'=>30, 'maxsize'=>100, 'title'=>'Job Description','header'=>true, 'mandatory'=>true, 'readonly'=>'true'),
     'jobstart'=>array('type'=>'isodate', 'inputstyle'=>'popup', 'required'=>'true', 'title'=>'Job Start Date', 'readonly'=>'true'),
     'jobend'=>array('type'=>'isodate', 'inputstyle'=>'popup', 'title'=>'Job End Date', 'readonly'=>'true'),
   );
 
+  /**
+  * overrides to standard field definitions for student users
+  * 
+  * @see $_field_defs
+  */
   static $_field_defs_student_override = array(
     'company_id'=>array('type'=>'lookup', 'header'=>true, 'object'=>'company', 'value'=>'name', 'title'=>'Company', 'var'=>'companies', 'readonly'=>'true'),
     'position'=>array('type'=>'text', 'size'=>30, 'maxsize'=>100, 'title'=>'Job Description','header'=>true, 'mandatory'=>true, 'readonly'=>'true'),
@@ -68,7 +87,9 @@ class Placement extends DTO_Placement
     'jobend'=>array('type'=>'isodate', 'inputstyle'=>'popup', 'title'=>'Job End Date', 'readonly'=>'true'),
   );
 
-
+  /**
+  * constructor that uses the default database for placement objects
+  */ 
   function __construct() 
   {
     parent::__construct('default');
@@ -85,7 +106,12 @@ class Placement extends DTO_Placement
     return(self::$_field_defs);
   }
 
-
+  /**
+  * load a given object by its id
+  * 
+  * @param int $id the unique id for the record
+  * @return a placement object, check id is non-zero to indicate success
+  */ 
   function load_by_id($id) 
   {
     $placement = new Placement;
@@ -94,6 +120,16 @@ class Placement extends DTO_Placement
     return $placement;
   }
 
+  /**
+  * inserts a new placement record in the database
+  * 
+  * This automatically marks the student as placed, sets the created timestamp
+  * on the placement record, and attempts to see if sufficient data is available
+  * and or changed to warrant creating a supervisor user or updating one.
+  * 
+  * @param array $fields an associative array of fields for the table
+  * @see Supervisor::update_from_placement()
+  */
   function insert($fields) 
   {
     // Null some fields if empty
@@ -102,11 +138,11 @@ class Placement extends DTO_Placement
     require_once('model/Student.class.php');
     // Record student as placed
     $student_fields['placement_status'] = 'Placed';
+    // inbound $student_id is the user id, not the id from the student table
     $student_fields['id'] = Student::get_id_from_user_id($fields['student_id']);
     Student::update($student_fields);
 
-    // Inbound student_id is not from user table
-    $fields['student_id'] = $fields['student_id'];
+    // Now the main insertion
     $fields['created'] = date("YmdHis");
     $placement = new Placement;
     $id = $placement->_insert($fields);
@@ -116,10 +152,23 @@ class Placement extends DTO_Placement
     Supervisor::update_from_placement($id, $fields);
   }
 
+  /**
+  * updates a placement record in the database
+  * 
+  * This protects key fields from alteration, and updated the modified 
+  * timestamp automatically. It also attempts to see if sufficient data is 
+  * available and or changed to warrant creating a supervisor user or updating
+  * one.
+  * 
+  * @param array $fields an associative array of fields for the table
+  * @see Supervisor::update_from_placement()
+  */
   function update($fields) 
   {
+    // Some fields should never change
     unset($fields['created']);
     unset($fields['company_id']);
+    unset($fields['student_id']);
     // Some fields cannot be reset by non admins
     if(!User::is_admin())
     {
@@ -145,6 +194,8 @@ class Placement extends DTO_Placement
 
   /**
   * Goes through certain fields and sets them to null if they are "empty"
+  * 
+  * @param array $fields an array of fieldnames to set to null
   */
   function set_empty_to_null($fields)
   {
@@ -157,7 +208,10 @@ class Placement extends DTO_Placement
   }
 
   /**
-  * Wasteful
+  * checks whether a given placement record exists
+  * 
+  * @param int $id the unique id to check for
+  * @return true if the record exists, false otherwise
   */
   function exists($id) 
   {
@@ -167,7 +221,10 @@ class Placement extends DTO_Placement
   }
 
   /**
-  * Wasteful
+  * counts the number of placement records that meet search criteria
+  * 
+  * @param string $where_clause an optional clause to limit the searched records
+  * @return the number of matching records
   */
   function count($where_clause="") 
   {
@@ -175,6 +232,12 @@ class Placement extends DTO_Placement
     return $placement->_count($where_clause);
   }
 
+  /**
+  * obtains the most recent placement for a specific student, if one exists
+  * 
+  * @param int $student_user_id the id from the user table for the student
+  * @return the placement object if found, false otherwise
+  */
   function get_most_recent($student_user_id)
   {
     $student_user_id = (int) $student_user_id;
@@ -185,6 +248,14 @@ class Placement extends DTO_Placement
     else return(false);
   }
 
+  /**
+  * get all placement records with certain criteria, with paging
+  * 
+  * @param string $where_clause the where clause for the query
+  * @param string $order_by the ordering criteria (defaults to id)
+  * @param page the page number (zero means initial search)
+  * @return array of placement objects
+  */
   function get_all($where_clause="", $order_by="ORDER BY id", $page=0)
   {
     global $config;
@@ -200,6 +271,13 @@ class Placement extends DTO_Placement
     return $placements;
   }
 
+  /**
+  * gets an array of the values of a given field, indexed by id
+  * 
+  * @param string $fieldname the field to fetch
+  * @param string $where_clause an optional where clause to limit the search
+  * @return an array of the field values indexed by the id field
+  */
   function get_id_and_field($fieldname, $where_clause="") 
   {
     $placement = new Placement;
@@ -208,19 +286,38 @@ class Placement extends DTO_Placement
     return $placement_array;
   }
 
-
+  /**
+  * removes a given placement record
+  * 
+  * @param int $id the unique id (defaults to zero for safety)
+  */ 
   function remove($id=0)
   {
     $placement = new Placement;
     $placement->_remove_where("WHERE id=$id");
   }
 
+  /**
+  * returns all the fieldnames in use
+  * 
+  * @param boolean $include_id include the id field in the list
+  * @return an array of fieldnames
+  */ 
   function get_fields($include_id = false) 
   {
     $placement = new Placement;
     return  $placement->_get_fieldnames($include_id);
   }
 
+  /**
+  * obtains all the fields from the $_REQUEST array
+  * 
+  * Some of these should be carefully dismissed in the relevant insert and
+  * update functions.
+  * 
+  * @param boolean $include_id include the id field in the list
+  * @return an associative array of field, value type
+  */ 
   function request_field_values($include_id = false)
   {
     $fieldnames = Placement::get_fields($include_id);
