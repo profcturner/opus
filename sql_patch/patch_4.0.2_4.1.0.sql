41fbc0ec1e7445a6ecc5ee97e487700df1cb8421
Colin Turner <c.turner@ulster.ac.uk>
2009-02-01 03:53:00 +0000

    Update help for new CSV mapping code
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@791 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

--- sql_patch/patch_4.0.2_4.1.0.sql
+++ sql_patch/patch_4.0.2_4.1.0.sql
@@ -24,4 +24,20 @@ insert into `automail` (language_id, lookup, fromh, toh, cch, bcch, subject, des
 delete from csvmapping where id=1;
 delete from csvmapping where id=2;
 
-INSERT INTO `csvmapping` VALUES ('University of Ulster Module List','/^Y([0-9]*),(B[0-9]*),\"([A-Za-z]*), ([A-Za-z]*)\",([0-9]*),([A-Z]*),(.*),(.*),(.*)$/','\"${1}\",\"${2}\",\"\",\"${4}\",\"${3}\",\"${9}\",\"${5}\",\"\",\"${2}\"','/^Year,.*$/',1),('University of Ulster Programme List','/^Y([0-9]*),(B[0-9]*),\"([A-Za-z\'\\-]*), ([A-Za-z\'\\-]*)\",(.*),(.*),\"(.*)\"$/ ','\"${1}\",\"${2}\",\"\",\"${4}\",\"${3}\",\"${6}\",\"\",\"\",\"${2}\"','/^Year,.*$/',2)
+INSERT INTO `csvmapping` VALUES ('University of Ulster Module List','/^Y([0-9]*),(B[0-9]*),\"([A-Za-z]*), ([A-Za-z]*)\",([0-9]*),([A-Z]*),(.*),(.*),(.*)$/','\"${1}\",\"${2}\",\"\",\"${4}\",\"${3}\",\"${9}\",\"${5}\",\"\",\"${2}\"','/^Year,.*$/',1),('University of Ulster Programme List','/^Y([0-9]*),(B[0-9]*),\"([A-Za-z\'\\-]*), ([A-Za-z\'\\-]*)\",(.*),(.*),\"(.*)\"$/ ','\"${1}\",\"${2}\",\"\",\"${4}\",\"${3}\",\"${6}\",\"\",\"\",\"${2}\"','/^Year,.*$/',2);
+
+
+CREATE TABLE `help` (
+  `language_id` int(10) unsigned default NULL,
+  `lookup` tinytext NOT NULL,
+  `channel_id` int(10) unsigned default NULL,
+  `auth` tinytext,
+  `description` tinytext,
+  `contents` text,
+  `id` int(10) unsigned NOT NULL auto_increment,
+  PRIMARY KEY  (`i
+  
+-- update help for new csv code --
+
+delete from help where lookup='RootManageCSVMapping' and language_id="1";
+insert into help (language_id, lookup, channel_id, auth, description, contents) values(1,'RootManageCSVMapping',0,'admin','More detailed help on how to set up a CSV Mapping','<p>CSV Mappings are usually a way of mapping your own University format CSV files to a \"standard\" format used by OPUS. Note however, that your original file format can really be any text file which has one student per line. It\'s just as practical to use TSV or similar formats.</p>\r\n<p>To set up a mapping, you will need to understand <a href=\"http://www.php.net/manual/en/reference.pcre.pattern.syntax.php\" target=\"blank\">regular expressions</a>, as used by the <a href=\"http://www.php.net/manual/en/function.preg-replace.php\" target=\"blank\">preg_replace</a> function in PHP. If you don\'t, it is strongly recommended you file a service ticket or ask a member of IT staff to help you set this up; regular expressions are a relatively complex concept if your format requires anything but the simplest manipulation.</p>\r\n<p>OPUS expects the replacement line to map to the following:</p>\r\n<p>\"year_of_study\",\r\n    \r\n    \r\n    \r\n    \r\n    <br />\"student_reg_number\",\r\n    \r\n    \r\n    \r\n    \r\n    <br />\"title\",\r\n    \r\n    \r\n    \r\n    \r\n    <br />\"firstname\",\r\n    \r\n    \r\n    \r\n    \r\n    <br />\"lastname\",\r\n    \r\n    \r\n    \r\n    \r\n    <br />\"email\",\r\n    \r\n    \r\n    \r\n    \r\n    <br />\"programme_code\",\r\n    \r\n    \r\n    \r\n    \r\n    <br />\"disability_code\",<br />\"username\"</p>\r\n<p>all on one line. Note that if you don\'t have information about one of these, you should include the quotes with no content \"\". This functionality is on this menu since errors here will generate errors in the student import functionality. If the username is empty, the registration number will be used for this also.</p>');
