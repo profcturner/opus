01bb6db830bcd325764fbeb6eb8b7d8b03a8ac07
Colin Turner <c.turner@ulster.ac.uk>
2009-02-13 03:02:56 +0000

    Reinstating authenticator but disabling it for security reasons
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@804 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

---
+++ include/auth.d/90_testauth.class.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+* Test authentication
+* @package OPUS
+*/
+
+/**
+* Test authentication
+*
+* @author Gordon Crawford <g.crawford@ulster.ac.uk>
+* @author Colin Turner <c.turner@ulster.ac.uk>
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License v2
+* @package OPUS
+*
+*/
+
+class testauth
+{
+  function waf_authenticate_user($username, $password)
+  {
+    // This authenticator should not be used and will be removed in another version
+    return(false);
+    switch ($username) 
+    {
+      case "admin" : 
+        $stub_user['valid'] = True;
+        $stub_user['username'] = 'admin';
+        $stub_user['user'] = array('firstname'=>'Colin', 'lastname'=>'Turner', 'email'=>'c.turner@ulster.ac.uk');
+        $stub_user['groups'] = array('admin');
+        break;
+      case "academic" :
+        $stub_user['valid'] = True;
+        $stub_user['user'] = array('firstname'=>'Colin', 'lastname'=>'Turner', 'email'=>'c.turner@ulster.ac.uk');
+        $stub_user['groups'] = array('academic');
+        break;
+      case "company" :
+        $stub_user['valid'] = True;
+        $stub_user['user'] = array('firstname'=>'Colin', 'lastname'=>'Turner', 'email'=>'c.turner@ulster.ac.uk');
+        $stub_user['groups'] = array('company');
+        break;
+      case "workplace" :
+        $stub_user['valid'] = True;
+        $stub_user['user'] = array('firstname'=>'Colin', 'lastname'=>'Turner', 'email'=>'c.turner@ulster.ac.uk');
+        $stub_user['groups'] = array('workplace');
+        break;
+      case "student" :
+        $stub_user['valid'] = True;
+        $stub_user['user'] = array('firstname'=>'Colin', 'lastname'=>'Turner', 'email'=>'c.turner@ulster.ac.uk');
+        $stub_user['groups'] = array('student');
+        $stub_user['reg_number'] = "X1000000";
+        $stub_user['user_id'] = "-1";
+        break;
+      case "super" :
+        $stub_user['valid'] = True;
+        $stub_user['user'] = array('firstname'=>'Colin', 'lastname'=>'Turner', 'email'=>'c.turner@ulster.ac.uk');
+        $stub_user['groups'] = array('super');
+        break;
+      case "guest" :
+        $stub_user['valid'] = True;
+        $stub_user['user'] = array('firstname'=>'Colin', 'lastname'=>'Turner', 'email'=>'c.turner@ulster.ac.uk');
+        $stub_user['groups'] = array('guest');
+        break;
+      case "multi" :
+        $stub_user['valid'] = True;
+        $stub_user['user'] = array('firstname'=>'Colin', 'lastname'=>'Turner', 'email'=>'c.turner@ulster.ac.uk');
+        $stub_user['groups'] = array('super', 'academic');
+        break;
+      default:
+        return false;
+    }
+    return $stub_user;
+  }
+}
+
+?>
