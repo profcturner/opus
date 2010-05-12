1086404152ca17b1b7cf8b229df9cac29ee5ced1
Colin Turner <c.turner@ulster.ac.uk>
2010-05-12 09:19:41 +0000

    Refining queries for copying results
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@853 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

--- include/dto/DTO_AssessmentTotal.class.php
+++ include/dto/DTO_AssessmentTotal.class.php
@@ -43,29 +43,27 @@ class DTO_AssessmentTotal extends DTO
     return $object_array;
   }
   
+  /**
+  * copies assessment totals from one regime item to another
+  * 
+  * This function is rarely used, and only for the rare and dangerous
+  * occaision that an assessment regime is changed mid flow. It allows
+  * all the totals to be copied from essentially the same assessment
+  * under a different regime item to its counterpart in a new regime
+  *
+  * @param $old_regime_id the regime_id of the old assessment
+  * @param $new_regime_id the regime_id of the new assessment
+  */  
   function _copy_results($old_regime_id, $new_regime_id)
   {
 	$waf =& UUWAF::get_instance();
     $con = $waf->connections[$this->_handle]->con;
 
-    $tmp_tablename = "tmp_at_" . $old_regime_id . "_" . $new_regime_id;
     try
     {
-      $query = "create temporary table ? type = heap select * from assessmenttotal where regime_id= ?";
+      $query = "insert into assessmenttotal (regime_id, assessed_id, assessor_id, comments, mark, outof, percentage, created, modified, assessed) select ?, assessed_id,assessor_id, comments, mark, outof, percentage, created, modified, assessed from assessmenttotal where regime_id=?";
       $sql = $con->prepare($query);
-      $sql->execute(array($tmp_tablename, $old_regime_id));
-      
-      $query = "update ? set regime_id = ? where regime_id = ?";
-      $sql = $con->prepare($query);
-      $sql->execute(array($tmp_tablename, $old_regime_id, $new_regime_id));
-
-      $query = "insert into assessmenttotal select * from ?";
-      $sql = $con->prepare($query);
-      $sql->execute(array($tmp_tablename));
-      
-      $query = "drop table ?";
-      $sql = $con->prepare($query);
-      $sql->execute(array($tmp_tablename));
+      $sql->execute(array($new_regime_id, $old_regime_id));
     }
     catch (PDOException $e)
     {
