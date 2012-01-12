1197d5021680b40cc6dcb0ddf86b7ecdec5a517e
Andrew Weekes <aj.weekes@ulster.ac.uk>
2012-01-12 15:23:08 +0000

    opus look and feel update
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@887 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

---
+++ include/dto/DTO_Student_Detail.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package PDSystem
+ *
+ *
+ */
+require_once("dto/DTO_Cache.class.php");
+
+class DTO_Student_Detail extends DTO 
+{  
+
+  function __construct($handle = 'default') 
+  {
+    parent::__construct($handle);
+  }
+
+}
+
+?>
