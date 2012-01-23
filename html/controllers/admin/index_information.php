86ccd9eed036af743e48c047d26d5e88b1573208
Andrew Weekes <aj.weekes@ulster.ac.uk>
2012-01-23 14:55:34 +0000

    removing thickbox from view_resource and setting about page to open in thickbox
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@920 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

--- html/controllers/admin/index_information.php
+++ html/controllers/admin/index_information.php
@@ -12,7 +12,7 @@
   {
     $opus->assign("nopage", true);
 
-    manage_objects($opus, $user, "Resource", array(), array(array('view', 'view_resource'), array('info','info_resource')), "get_all", array("where company_id = 0 or company_id is null"), "admin:information:list_resources:list_resources");
+    manage_objects($opus, $user, "Resource", array(), array(array('view', 'view_resource', 'no'), array('info','info_resource')), "get_all", array("where company_id = 0 or company_id is null"), "admin:information:list_resources:list_resources");
   }
 
   function view_resource(&$opus, $user, $title)
@@ -223,7 +223,7 @@
   {
     $waf->assign("show_banners", true);
     $waf->assign("ulster_logo", true);
-    $waf->display("bounded.tpl", "general:information:information:about", "general/information/about.tpl");
+    $waf->display("popup.tpl", "general:information:information:about", "general/information/about.tpl");
   }
 
   function privacy(&$waf)
