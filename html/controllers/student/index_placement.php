df33ef5c5811ef14c27baef09994dec852f66550
Andrew Weekes <aj.weekes@ulster.ac.uk>
2012-01-30 15:32:22 +0000

    thickbox display fix
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@949 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

--- html/controllers/student/index_placement.php
+++ html/controllers/student/index_placement.php
@@ -218,7 +218,7 @@
     $resource = Resource::load_by_id($id);
 
     $opus->assign("resource", $resource);
-    $opus->display("main.tpl", "student:placement:info_resource:info_resource", "general/information/info_resource.tpl");
+    $opus->display("popup.tpl", "student:placement:info_resource:info_resource", "general/information/info_resource.tpl");
   }
 
 
