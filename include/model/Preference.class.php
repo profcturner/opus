db8544e3681060a66dab105fe09c7121f0617f72
Andrew Weekes <aj.weekes@ulster.ac.uk>
2012-03-07 13:00:43 +0000

    OPUS and PACE share the same theme
    git-svn-id: svn+ssh://foss.ulster.ac.uk/svn/opus/trunk@969 54dc3e3f-df17-4cd7-99f1-b1610cd3c088

--- include/model/Preference.class.php
+++ include/model/Preference.class.php
@@ -45,19 +45,19 @@ class Preference extends DTO_Preference
     return($preferences);
   }
 
-  function load_all($user_id)
+  function load_all($preference_id)
   {
     $waf =& UUWAF::get_instance();
     $application = $waf->title;
-    if(empty($user_id))
+    if(empty($preference_id))
     {
-      $waf->log("preferences cannot be loaded for this without a reg_number", PEAR_LOG_DEBUG, 'debug');
+      $waf->log("preferences cannot be loaded for this without a reg number", PEAR_LOG_DEBUG, 'debug');
       return;
     }
 
     $pref = new Preference;
 
-    $preferences = $pref->_get_all("where application='$application' and reg_number='$user_id'");
+    $preferences = $pref->_get_all("where application='$application' and reg_number='$preference_id'");
 
     // Unwind into the session
     foreach($preferences as $preference)
@@ -67,26 +67,36 @@ class Preference extends DTO_Preference
     $waf->log(count($preferences) . " preference values loaded", PEAR_LOG_DEBUG, 'debug');
   }
 
-  function get_system_theme($user_id)
+  function get_system_theme($preference_id)
   {
     $waf =& UUWAF::get_instance($config['waf']);
 
     $application = $waf->title;
   
-    if(empty($user_id))
+    if(empty($preference_id))
     {
-      $waf->log("preferences cannot be loaded for this user without a user id", PEAR_LOG_DEBUG, 'debug');
+      $waf->log("preferences cannot be loaded for this user without a reg number", PEAR_LOG_DEBUG, 'debug');
       $system_theme = "blue";
       return $system_theme;
     }
 
 		if(empty($application))
     {
-      $waf->log("preferences cannot be loaded for  user id".$user_id." without an application name", PEAR_LOG_DEBUG, 'debug');
+      $waf->log("preferences cannot be loaded for user ".$preference_id." without an application name", PEAR_LOG_DEBUG, 'debug');
       $system_theme = "blue";
       return $system_theme;
     }
 
+	$pref = new Preference;
+
+    $preferences = $pref->_get_all("where application='$application' and reg_number='$preference_id'");
+
+    // Unwind into the session
+    foreach($preferences as $preference)
+    {
+      Preference::set_preference($preference->name, unserialize($preference->value));
+    }
+	
 	$preferences = $_SESSION['waf'][$waf->title]['preferences'];
 
 	$system_theme = $preferences['system_theme'];
@@ -98,44 +108,65 @@ class Preference extends DTO_Preference
     return $system_theme;
   }
 
-  function save_all($user_id)
+  function save_all($preference_id)
   {
+    require_once("model/User.class.php");
     $waf =& UUWAF::get_instance();
     $application = $waf->title;
 
-
-    if(empty($user_id))
+    if(empty($preference_id))
     {
-      $waf->log("preferences cannot be saved for this user without a user id", PEAR_LOG_DEBUG, 'debug');
+      $waf->log("preferences cannot be saved for this user without a reg number", PEAR_LOG_DEBUG, 'debug');
       return;
     }
     // Nothing to save?
     if(!isset($_SESSION['waf'][$application]['preferences'])) return;
+    
 
     // Remove what's there
     $pref = new Preference; 
-    //$pref->_remove_where("where application='$application' and reg_number='$user_id'");
-    $pref->_remove_where("where application='$application' and reg_number='$user_id'");
+    //$pref->_remove_where("where application='$application' and reg_number='$preference_id'");
+    $pref->_remove_where("where application='$application' and reg_number='$preference_id'");
+    $pref->_remove_where("where application='pace' and reg_number='$preference_id' and name='system_theme'");
     $count = 0;
 
     foreach($_SESSION['waf'][$application]['preferences'] as $name => $value)
     {
       $fields = array();
       $fields['application'] = $application;
-      $fields['reg_number'] = $user_id;
+      $fields['reg_number'] = $preference_id;
       $fields['name'] = $name;
       $fields['value'] = serialize($value);
 
       $pref->_insert($fields);
       $count++;
+        //Write the same theme for PACE
+		if($name == 'system_theme')
+		{ 
+		  $pace_fields = array();
+		  
+		  $pace_fields['application'] = 'pace';
+		  $pace_fields['reg_number'] = $preference_id;
+		  $pace_fields['name'] = $name;
+		  $pace_fields['value'] = serialize($value);
+		  $pref->_insert($pace_fields);
+		}
     }
-    $waf->log("$count preference values saved", PEAR_LOG_DEBUG, 'debug');
+    $waf->log("$count preference values saved $preference_id", PEAR_LOG_DEBUG, 'debug');
   }
 
   function set_preference($name, $value)
   {
     $waf =& UUWAF::get_instance();
     $application = $waf->title;
+
+    $_SESSION['waf'][$application]['preferences'][$name] = $value;
+  }
+
+  function set_theme($name, $value)
+  {
+     $waf =& UUWAF::get_instance($config['waf']);
+    $application = 'pace';
 
     $_SESSION['waf'][$application]['preferences'][$name] = $value;
   }
